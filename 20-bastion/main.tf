05f4bcd9d1c69461a38eb489272698a58aa14df6
kalyantalbothula <kalyan6166@gmail.com>
2025-08-19 23:11:43 +1000

    terraform

--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -3,7 +3,7 @@
 resource "aws_instance" "bastion" {
   ami           = local.ami_id
   instance_type = "t3.micro"
-  vpc_security_group_ids = local.bastion_sg_id
+  vpc_security_group_ids = [local.bastion_sg_id]  # <-- [] for StringList
   subnet_id = local.public_subnet_id
 
   tags = merge(
