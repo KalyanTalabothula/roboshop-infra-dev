18cdf10ce7247a6d06d40ab00677adeb9ea8159b
kalyantalbothula <kalyan6166@gmail.com>
2025-08-20 15:53:22 +1000

    terraform

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -21,6 +21,17 @@ module "bastion" {
     vpc_id = local.vpc_id
 }
 
+module "backend_alb" {
+    #source = "../../terraform-aws-securitygroup"
+    source = "git::https://github.com/KalyanTalabothula/terraform-aws-securitygroup.git?ref=main"
+    project = var.project
+    environment = var.environment
+    
+    sg_name = "backend-alb"
+    sg_description = "for backend alb"
+    vpc_id = local.vpc_id
+}
+
 # 🔍 aws security group rule terraform, i mean ingress
 # bastion accepting connection from my laptop, ade miru office lo ite mii network istaru anamata, I mean CIDR
 
@@ -35,3 +46,17 @@ resource "aws_security_group_rule" "bastion_laptop" {
 }
 
 # security_group_id = module.bastion.sg_id, manam terraform-aws-securitygroup module use chesukoni bastion ki oka security create chestunnam, Now roboshop-infra-dev lo sg ane team vallu, terraform-aws-securitygroup ane module use chesukoni sg okka output tisukuntunnaruu. So bastion ee sg_id ni attach chesukovali ante, ee sg_id ni yela evvali.. 
+
+# Backend ALB accepting connections from my bastion host number port 80
+
+resource "aws_security_group_rule" "backend_alb_bastion" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  source_security_group_id = module.bastion.sg_id
+  # ipv6_cidr_blocks  = [aws_vpc.example.ipv6_cidr_block] No need for US
+  security_group_id = module.backend_alb.sg_id 
+}
+
+# cidr_blocks = ["0.0.0.0/0"] kadu ekkada, either sources security group ina evvali or Cide_blocks ina evvali.. source yekkad nimdi vastumdhi, Traffic source bastion nimdi kada, so i can give bastion 
