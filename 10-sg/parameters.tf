d2ab2a5e515b99d3c90b5485c3095a92ef6f3db7
kalyantalbothula <kalyan6166@gmail.com>
2025-08-21 13:14:11 +1000

    terraform

--- 10-sg/parameters.tf
+++ 10-sg/parameters.tf
@@ -20,5 +20,5 @@ resource "aws_ssm_parameter" "backend_alb_sg_id" {
 resource "aws_ssm_parameter" "vpn_sg_id" { 
   name  = "/${var.project}/${var.environment}/vpn_sg_id" 
   type  = "String"
-  value = module.vpn_sg_id 
+  value = module.vpn.sg_id 
 }
