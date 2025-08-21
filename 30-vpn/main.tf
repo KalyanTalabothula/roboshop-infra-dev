73df15cc539338e59c5978b848c7d028b200f5ee
kalyantalbothula <kalyan6166@gmail.com>
2025-08-21 22:33:37 +1000

    terraform

--- 30-vpn/main.tf
+++ 30-vpn/main.tf
@@ -14,7 +14,7 @@ resource "aws_instance" "vpn" {
   subnet_id = local.public_subnet_id
   #key_name = "daws-84" --> make sure this key exist in AWS
   key_name = aws_key_pair.openvpn.key_name 
-  #user_data = file("openvpn.sh")
+  user_data = file("openvpn.sh")
 
   tags = merge(
     local.common_tags,
