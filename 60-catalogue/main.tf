15de908562c1a5bedc7dc4ae150664290d5776b9
kalyantalbothula <kalyan6166@gmail.com>
2025-08-26 23:54:08 +1000

    updating

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -72,6 +72,13 @@ resource "aws_ami_from_instance" "catalogue" {
   name               = "${var.project}-${var.environment}-catalogue"
   source_instance_id = aws_instance.catalogue.id 
   depends_on = [ aws_ec2_instance_state.catalogue ]  # instance mottam stop ina tarawata, nenu AMI tisukovali. 
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project}-${var.environment}-catalogue"
+    }
+  )
 }
 
 # 🔍 aws command line to terminate ec2
