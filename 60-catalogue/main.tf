98f1d85175b5d4de0cd5b23185823f2e32d12d17
kalyantalbothula <kalyan6166@gmail.com>
2025-08-26 23:06:59 +1000

    updating

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -63,7 +63,7 @@ resource "terraform_data" "catalogue" {
 # 🔍 aws_ec2_instance_state
 resource "aws_ec2_instance_state" "catalogue" {
   instance_id = aws_instance.catalogue.id
-  state       = "stopped"
+  state       = "stopped"   # we have 2-options stopped & running only, NO DELETE option. 
   depends_on = [ terraform_data.catalogue ]
 }
 
