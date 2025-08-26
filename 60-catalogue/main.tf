f64419a3304f128bdfca8cdb0de8f4542cd73b5b
kalyantalbothula <kalyan6166@gmail.com>
2025-08-26 23:12:40 +1000

    updating

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -73,3 +73,30 @@ resource "aws_ami_from_instance" "catalogue" {
   source_instance_id = aws_instance.catalogue.id 
   depends_on = [ aws_ec2_instance_state.catalogue ]  # instance mottam stop ina tarawata, nenu AMI tisukovali. 
 }
+
+resource "terraform_data" "catalogue_delete" {
+  triggers_replace = [
+    aws_instance.catalogue.id  
+  ]
+  
+  provisioner "file" {
+    source      = "catalogue.sh"
+    destination = "/tmp/catalogue.sh"
+  }
+
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"     
+    password = "DevOps321"
+    host     = aws_instance.catalogue.private_ip    
+  }
+
+    provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/catalogue.sh",
+      "sudo sh /tmp/catalogue.sh catalogue ${var.environment}"
+    ]
+  }
+
+  depends_on = [ aws_ami_from_instance.catalogue ] # aws instance create ina taruwata gha 
+}
