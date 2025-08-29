2a72a6117e162c3baebda4644bb4e00a741fa888
kalyantalbothula <kalyan6166@gmail.com>
2025-08-29 14:33:55 +1000

    updating

--- 60-acm/main.tf
+++ 60-acm/main.tf
@@ -15,7 +15,8 @@ resource "aws_acm_certificate" "kalyanu" {    # <--
   }
 }
 
-# next we need validation we need DNS not Email. Now it create records 
+# next we need validation we need DNS not Email. Now it create records. 
+# certificates lo nimdi tisukoni adi records anedi create chesukuntumdhi.
 resource "aws_route53_record" "kalyanu" {
   for_each = {
     for dvo in aws_acm_certificate.kalyanu.domain_validation_options : dvo.domain_name => {  # <--
@@ -32,3 +33,15 @@ resource "aws_route53_record" "kalyanu" {
   type            = each.value.type
   zone_id         = var.zone_id         # <--
 }
+
+# certificate create chusukunna taruwata validation ane button click cheyali
+#  🔍 aws acm certificate validation 
+resource "aws_acm_certificate_validation" "kalyanu" {
+  certificate_arn         = aws_acm_certificate.kalyanu.arn
+  validation_record_fqdns = [for record in aws_route53_record.kalyanu : record.fqdn]
+}
+# ante ee certificate ki ekkada create chesina records ni validate chestumdhi.. 
+
+# manam export cheyali gha certificate ni, export cheste manam dhanini use chesukovachhu. so export cheyali ante we need ARN-Amazon resource name
+
+# certificate create avutumdhi--> Records create avutunnae --> tarawata validate jarugutumdhi..
