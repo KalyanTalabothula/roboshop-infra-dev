73b2a95382323d81fbacfd5650286d4f14f767e0
kalyantalbothula <kalyan6166@gmail.com>
2025-09-01 14:50:21 +1000

    updating

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -1,6 +1,6 @@
 module "component" {
     for_each = var.components
-    source = "../../terraform-aws-roboshop" # taking catalogue as module now 
+    source = "git::https://github.com/KalyanTalabothula/terraform-aws-roboshop.git?ref=main" # taking catalogue as module now 
     component = each.key
     rule_priority = each.value.rule_priority
 }
